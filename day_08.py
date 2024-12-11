cc7b95aaec1143cd18c93c3b500617881fc51075
Jackie Liu <jackolake@gmail.com>
2024-12-11 08:30:07 +0800

    Day 08

--- day_08.py
+++ day_08.py
@@ -3,14 +3,23 @@ from itertools import combinations
 with open('input/day_08.txt') as f:
     G = {(x, y): v for y, row in enumerate(f.read().splitlines()) for x, v in enumerate(row)}
 antennas = {a: [coord for coord, v in G.items() if v == a] for a in set(G.values()) if a.isalnum()}
-antinodes = set()
 
-for antenna, coords in antennas.items():
-    for (x1, y1), (x2, y2) in combinations(coords, r=2):
-        x_delta, y_delta = x2 - x1, y2 - y1
-        for (x_try, y_try) in [(x1 - x_delta, y1 - y_delta), (x2 + x_delta, y2 + y_delta)]:
-            if (x_try, y_try) in G:
-                antinodes.add((x_try, y_try))
+def run(part2: bool) -> int:
+    antinodes = set()
+    start, end, step = (-100, 100, 1) if part2 else (-2, 2, 3)
+    for coords in antennas.values():
+        for (x1, y1), (x2, y2) in combinations(coords, r=2):
+            x_delta, y_delta = x2 - x1, y2 - y1
+            for i in range(start, end, step):
+                x_try, y_try = (x2 + i * x_delta, y2 + i * y_delta)
+                if (x_try, y_try) in G:
+                    antinodes.add((x_try, y_try))
+    return len(antinodes)
+
 # Part 1
-part1_answer = len(antinodes)
+part1_answer = run(part2=False)
 print(f'Part 1: {part1_answer}')
+
+# Part 2
+part2_answer = run(part2=True)
+print(f'Part 2: {part2_answer}')
